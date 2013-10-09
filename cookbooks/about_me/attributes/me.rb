a560982214a9979781032a8d78f80c32473676a0
deanonishi <dean@rightscale.com>
2013-10-09 20:22:17 +0000

    Added the about_me cookbook

---
+++ cookbooks/about_me/attributes/me.rb
@@ -0,0 +1,6 @@
+
+set[:about_me][:first_name] = "Deano"
+default[:about_me][:favorite][:food] = "pizza"
+default[:about_me][:favorite][:drink] = "water"
+default[:about_me][:favorite][:hobby] = "running"
+
